8e7301e56df51eef6ebefaa59757964a9079ab2e
AbdulkaderFreij <abdul.freij@gmail.com>
2019-12-16 15:26:57 +0200

    step8

---
+++ Exercises/Javascript-Basics-01/Step_8.js
@@ -0,0 +1,6 @@
+function result() {
+  var age = document.getElementById("age").value;
+  if (age > 18) alert(" you are over 18");
+  else if (age == 18) alert("you are 18!!!");
+  else alert("you are under 18");
+}
