a163c88b78214932c90da72a9d901a274243b54d
AbdulkaderFreij <abdul.freij@gmail.com>
2019-12-16 13:50:21 +0200

    step4

---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,10 @@
+function input() {
+  alert(
+    "name: " +
+      document.getElementById("name").value +
+      "\n surname: " +
+      document.getElementById("surname").value +
+      " \nville: " +
+      document.getElementById("city").value
+  );
+}
