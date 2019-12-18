517de383621e3ecb1ade6e7480465bdc2aacbafe
AbdulkaderFreij <abdul.freij@gmail.com>
2019-12-18 16:36:36 +0200

    step3

---
+++ Exercises/Javascript-Basics-03/step 3/step_3.js
@@ -0,0 +1,11 @@
+document.querySelector(".green").onclick = function() {
+  document.querySelector("p").style.color = "green";
+};
+
+document.querySelector(".red").onclick = function() {
+  document.querySelector("p").style.color = "red";
+};
+
+document.querySelector(".blue").onclick = function() {
+  document.querySelector("p").style.color = "blue";
+};
