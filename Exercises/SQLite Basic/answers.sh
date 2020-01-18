c77958b8aae4ad403f1ac1af4023a44c18161adf
AbdulkaderFreij <abdul.freij@gmail.com>
2020-01-18 19:40:14 +0200

    basic queries

---
+++ Exercises/SQLite Basic/answers.sh
@@ -0,0 +1,32 @@
+problem0: "select * from students"
+
+Basic Queries:
+
+1- SELECT Name
+   FROM students;
+
+2- SELECT *
+   FROM students
+   WHERE Age>30
+
+3- SELECT Name
+   FROM students
+   WHERE Age=30 AND Gender='F'
+
+4- SELECT Points
+   FROM students
+   WHERE name= "Alex"
+
+5- INSERT INTO students(ID, Name, Age, Gender, Points)
+   VALUES (8, 'Abdulkader', 28, 'M', 600)
+
+6- UPDATE students SET Points=400 WHERE ID=2
+
+7- UPDATE students SET Points=100 WHERE ID=1
+
+8- UPDATED!!
+
+9- COMMITED!!
+
+
+
