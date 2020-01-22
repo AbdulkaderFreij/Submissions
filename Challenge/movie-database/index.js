fa30ec3be85e2edbfc98244d6c4918c9de78baf6
AbdulkaderFreij <abdul.freij@gmail.com>
2020-01-22 13:14:05 +0200

    Step3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -8,6 +8,16 @@ app.get("/", (req, res) => {
   res.send("ok");
 });
 
+app.get("/test", (req, res) => {
+  res.json({ status: 200, message: "ok" });
+});
+
+app.get("/time", (req, res) => {
+  var dt = new Date();
+  //  dt.setHours( dt.getHours() + 2 );
+  res.json({ status: 200, message: `${dt.getHours()}:${dt.getMinutes()}` });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}/`);
 });
