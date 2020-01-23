f3700eb497022415dddbd04e29982baac5d24894
AbdulkaderFreij <abdul.freij@gmail.com>
2020-01-23 09:03:23 +0200

    modified answers.sh

--- Exercises/SQLite Basic/answers.sh
+++ Exercises/SQLite Basic/answers.sh
@@ -1,91 +1,52 @@
-problem0: "select * from students"
-
-Basic Queries:
-
-1- SELECT Name
-   FROM students;
-
-2- SELECT *
-   FROM students
-   WHERE Age>30
-
-3- SELECT Name
-   FROM students
-   WHERE Age=30 AND Gender='F'
-
-4- SELECT Points
-   FROM students
-   WHERE name= "Alex"
-
-5- INSERT INTO students(ID, Name, Age, Gender, Points)
-   VALUES (8, 'Abdulkader', 28, 'M', 600)
-
-6- UPDATE students SET Points=400 WHERE ID=2
-
-7- UPDATE students SET Points=100 WHERE ID=1
-
-8- UPDATED!!
-
-9- COMMITED!!
 
 
+"SELECT Name FROM students"
 
-Creating Table:
+"SELECT * FROM students WHERE Age>30"
 
-CREATE TABLE graduates (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Name Text Not Null Unique, Age INTEGER, Gender TEXT, Points INTEGER, Graduation TEXT Date)
+"SELECT Name FROM students WHERE Age=30 AND Gender='F'"
 
-1- INSERT INTO graduates (ID, Name, Age, Gender, Points)
-   SELECT ID, Name, Age, Gender, Points
-   FROM students where ID=4
+"SELECT Points FROM students WHERE name= "Alex""
 
-2- UPDATE graduates SET Graduation="08/09/2018" WHERE ID=4
+"INSERT INTO students(ID, Name, Age, Gender, Points) VALUES (8, 'Abdulkader', 28, 'M', 600)"
 
-3- DELETE FROM students WHERE ID=4
+"UPDATE students SET Points=400 WHERE ID=2"
 
-4- COMMITED !!
+"UPDATE students SET Points=100 WHERE ID=1"
 
 
 
 
-Joins:
+"CREATE TABLE graduates (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Name Text Not Null Unique, Age INTEGER, Gender TEXT, Points INTEGER, Graduation TEXT Date)"
 
-1- CREATE TABLE newTable (Name Text Not Null Unique, Company TEXT, Date INTEGER Not Null)
+"INSERT INTO graduates (ID, Name, Age, Gender, Points) SELECT ID, Name, Age, Gender, Points FROM students where ID=4"
+
+"UPDATE graduates SET Graduation="08/09/2018" WHERE ID=4"
+
+"DELETE FROM students WHERE ID=4"
+
+
+
+
+"CREATE TABLE newTable (Name Text Not Null Unique, Company TEXT, Date INTEGER Not Null)"
    
-   INSERT INTO newTable (Name, Company, Date)
-   SELECT employees.Name, companies.Name, companies.Date 
-   FROM employees JOIN companies
-   ON companies.Name = employees.Company
+"INSERT INTO newTable (Name, Company, Date) SELECT employees.Name, companies.Name, companies.Date FROM employees JOIN companies ON companies.Name = employees.Company"
 
-2- Select Name FROM newTable WHERE Date<2000
+"Select Name FROM newTable WHERE Date<2000"
 
-3- Select Company FROM employees WHERE Role= "Graphic Designer"
+"Select Company FROM employees WHERE Role= "Graphic Designer""
  
-4- COMMITTED!!
-
-
-
-Count & Filter:
-
-1- SELECT * FROM students WHERE Points= (SELECT MAX(Points) FROM students)
-
-2- SELECT avg(Points) FROM students
-
-3- SELECT COUNT(*) FROM students WHERE Points=500
-
-4- SELECT * FROM students WHERE Name like "%s%"
-
-5-SELECT * FROM students ORDER BY Points DESC;
-
-6- COMMITTED!!
 
 
 
 
+"SELECT * FROM students WHERE Points= (SELECT MAX(Points) FROM students)"
 
+"SELECT avg(Points) FROM students"
 
+"SELECT COUNT(*) FROM students WHERE Points=500"
 
+"SELECT * FROM students WHERE Name like "%s%""
 
-
-
-
+"SELECT * FROM students ORDER BY Points DESC;"
 
